367b7b2291f54d0ef44b7e631bfdbe28593a10f7
Gonçalo Ferreira <gonff@users.noreply.github.com>
2018-12-04 19:20:52 +0000

    Update TP2main.m

--- TP2main.m
+++ TP2main.m
@@ -1,16 +1,14 @@
 filename = 'C:\Users\gon_f\OneDrive\Documentos\Uni\4ºano\TI\TP2\Data\EMG_norm.csv';
-x = csvread(filename)
+data = csvread(filename)
 
-%noise = randn(50000,1);
-%x = filter(1,[1 1/2 1/3 1/4],noise);
-%x = x(45904:50000);
+%entropia(data)
 
-a = lpc(x,3);
-est_x = filter([0 -a(2:end)],1,x);
-e = x-est_x;
+a = lpc(data,3);
+est_data = filter([0 -a(2:end)],1,data);
+e = data-est_data;
 [acs,lags] = xcorr(e,'coeff');
 
-plot(1:97,x(4001:4097),1:97,est_x(4001:4097),'--'), grid
+plot(1:97,data(4001:4097),1:97,est_data(4001:4097),'--'), grid
 title 'Original Signal vs. LPC Estimate'
 xlabel 'Sample number', ylabel 'Amplitude'
 legend('Original signal','LPC estimate')
