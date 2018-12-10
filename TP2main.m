d5a57226b838134b33936aaa5987966b05ca824a
Gonçalo Ferreira <gonff@users.noreply.github.com>
2018-12-10 15:39:46 +0000

    sssssss

--- TP2main.m
+++ TP2main.m
@@ -7,14 +7,6 @@ alpha=unique(Y);
 h=hist(Y,alpha);
 ent=entropy(Y)
 
+linearpredictor(Y);
 
-a = lpc(data,3);
-est_data = filter([0 -a(2:end)],1,data);
-e = data-est_data;
-[acs,lags] = xcorr(e,'coeff');
-
-
-plot(1:97,data(4001:4097),1:97,est_data(4001:4097),'--'), grid
-title 'Original Signal vs. LPC Estimate'
-xlabel 'Sample number', ylabel 'Amplitude'
-legend('Original signal','LPC estimate')
+adaptativelinear(Y)
